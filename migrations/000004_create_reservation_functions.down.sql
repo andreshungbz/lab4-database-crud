8535f20dea12382d39e1defe247b88b9768e7cc4
Andres Hung <2018118240@ub.edu.bz>
2026-02-20 23:57:06 -0600

    Refactor reservation functions and add adjust documentation

--- migrations/000004_create_reservation_functions.down.sql
+++ migrations/000004_create_reservation_functions.down.sql
@@ -1,12 +1,47 @@
 -- migrations/000004_create_reservation_functions.down.sql
 -- Drops all functions created for reservations in reverse order of their creation.
 
-DROP FUNCTION IF EXISTS fn_create_reservation_workflow(
-    INT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT,
-    INT, DATE, DATE, payment_method, reservation_source, BIGINT
+DROP FUNCTION IF EXISTS fn_reservation_workflow(
+    BIGINT,
+    INT,
+    DATE,
+    DATE,
+    payment_method,
+    reservation_source,
+    INT,
+    INT,
+    BIGINT
 );
 
-DROP FUNCTION IF EXISTS fn_create_registration(BIGINT, INT, INT);
-DROP FUNCTION IF EXISTS fn_find_available_room(INT, INT, DATE, DATE);
-DROP FUNCTION IF EXISTS fn_create_reservation_base(BIGINT, DATE, DATE, INT, payment_method, reservation_source);
-DROP FUNCTION IF EXISTS fn_get_or_create_guest(TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT, TEXT);
+DROP FUNCTION IF EXISTS fn_create_registration(
+    BIGINT,
+    INT,
+    INT
+);
+
+DROP FUNCTION IF EXISTS fn_create_reservation_base(
+    BIGINT,
+    DATE,
+    DATE,
+    payment_method,
+    reservation_source,
+    INT
+);
+
+DROP FUNCTION IF EXISTS fn_get_or_create_guest(
+    TEXT, CITEXT, TEXT,
+    TEXT, TEXT, TEXT, TEXT, TEXT
+);
+
+DROP FUNCTION IF EXISTS fn_calculate_payment(
+    INT,
+    DATE,
+    DATE
+);
+
+DROP FUNCTION IF EXISTS fn_find_available_room(
+    INT,
+    INT,
+    DATE,
+    DATE
+);
--- migrations/000004_create_reservation_functions.up.sql
+++ migrations/000004_create_reservation_functions.up.sql
@@ -2,21 +2,106 @@
 -- Creates functions for creating reservations.
 
 -- ====================================================================================
--- FUNCTION fn_get_or_create_guest returns the id of an existing guest
--- or a newly created one.
+-- FUNCTION fn_find_available_room returns the first row that satisfies availability
+-- according to the date range. The lowest room number is selected first.
+-- ====================================================================================
+
+CREATE OR REPLACE FUNCTION fn_find_available_room(
+    p_hotel_id INT,
+    p_room_type_id INT,
+    p_checkin DATE,
+    p_checkout DATE
+)
+RETURNS TABLE (
+    hotel_id INT,
+    room_number INT
+)
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT r.hotel_id, r.number
+    FROM room r
+    WHERE r.hotel_id = p_hotel_id
+        AND r.room_type_id = p_room_type_id
+        AND r.status_code = 'V/C' -- only consider vacant/clean rooms
+        AND NOT EXISTS (
+            SELECT 1
+            FROM registration reg
+            JOIN reservation res ON res.id = reg.reservation_id
+            WHERE reg.hotel_id = r.hotel_id
+              AND reg.room_number = r.number
+              AND res.canceled = FALSE -- ignore canceled reservations
+              AND res.checkout_date > p_checkin -- overlapping check
+              AND res.checkin_date < p_checkout
+        )
+    ORDER BY r.number
+    FOR UPDATE SKIP LOCKED  -- safe for concurrent allocations
+    LIMIT 1;
+END;
+$$ LANGUAGE plpgsql;
+
+-- ====================================================================================
+-- FUNCTION fn_calculate_payment takes a room types base rate and multiplies it
+-- by the number of nights defined from the checkin and checkout dates.
+-- ====================================================================================
+
+CREATE OR REPLACE FUNCTION fn_calculate_payment(
+    -- room attribute
+    p_room_type_id INT,
+    -- reservation attributes
+    p_checkin DATE,
+    p_checkout DATE
+)
+RETURNS NUMERIC AS $$
+DECLARE
+    v_days INT;
+    v_base_rate NUMERIC(12, 2);
+BEGIN
+    -- get room type's base rate
+    SELECT base_rate INTO v_base_rate
+    FROM room_type
+    WHERE id = p_room_type_id;
+
+    -- handle non-existent room types
+    IF NOT FOUND THEN
+        RAISE EXCEPTION '[nonexistent-room-type] Room type % does not exist', p_room_type_id;
+    END IF;
+
+    -- calculate days
+    -- checkout_date > checkin_date business rule in schema ensures this is at least 1
+    v_days := p_checkout - p_checkin;
+    RETURN v_base_rate * v_days;
+END;
+$$ LANGUAGE plpgsql;
+
+-- ====================================================================================
+-- FUNCTION fn_get_or_create_guest returns the guest and person data for an existing
+-- guest or a newly created guest based on the passed passport.
 -- ====================================================================================
 
 CREATE OR REPLACE FUNCTION fn_get_or_create_guest(
+    -- guest attributes
     p_passport TEXT,
-    p_contact_email TEXT,
+    p_contact_email CITEXT,
     p_contact_phone TEXT,
+    -- person attributes
     p_name TEXT,
     p_gender TEXT,
     p_street TEXT,
     p_city TEXT,
     p_country TEXT
 )
-RETURNS BIGINT
+RETURNS TABLE (
+    id BIGINT,
+    passport_number TEXT,
+    contact_email CITEXT,
+    contact_phone TEXT,
+    name TEXT,
+    gender TEXT,
+    street TEXT,
+    city TEXT,
+    country TEXT
+)
 AS $$
 DECLARE
     v_guest_id BIGINT;
@@ -27,73 +112,56 @@ BEGIN
     FROM guest g
     WHERE g.passport_number = p_passport;
 
-    -- return guest id if it already exists
-    IF v_guest_id IS NOT NULL THEN
-        RETURN v_guest_id;
+    -- if not found, create guest
+    IF v_guest_id IS NULL THEN
+        BEGIN
+            INSERT INTO person (name, gender, street, city, country)
+            VALUES (p_name, p_gender, p_street, p_city, p_country)
+            RETURNING person.id INTO v_guest_id;
+
+            INSERT INTO guest (id, passport_number, contact_email, contact_phone)
+            VALUES (v_guest_id, p_passport, p_contact_email, p_contact_phone);
+
+        EXCEPTION WHEN unique_violation THEN
+            -- handle concurrent insert
+            SELECT g.id
+            INTO v_guest_id
+            FROM guest g
+            WHERE g.passport_number = p_passport;
+        END;
     END IF;
 
-    -- if guest does not exist, then create one
-    BEGIN
-        INSERT INTO person (name, gender, street, city, country)
-        VALUES (p_name, p_gender, p_street, p_city, p_country)
-        RETURNING id INTO v_guest_id;
-
-        INSERT INTO guest (id, passport_number, contact_email, contact_phone)
-        VALUES (v_guest_id, p_passport, p_contact_email, p_contact_phone);
-
-        RETURN v_guest_id;
-
-    -- concurrent insert check
-    EXCEPTION WHEN unique_violation THEN
-        SELECT id INTO v_guest_id
-        FROM guest
-        WHERE passport_number = p_passport;
-
-        RETURN v_guest_id;
-    END;
+    -- return full guest + person data
+    RETURN QUERY
+    SELECT
+        g.id,
+        g.passport_number,
+        g.contact_email,
+        g.contact_phone,
+        p.name,
+        p.gender,
+        p.street,
+        p.city,
+        p.country
+    FROM guest g
+    JOIN person p ON p.id = g.id
+    WHERE g.id = v_guest_id;
 END;
 $$ LANGUAGE plpgsql;
 
 -- ====================================================================================
--- FUNCTION fn_calculate_payment takes a room types base rate and multiplies it
--- by the number of nights defined from the checkin and checkout dates.
--- ====================================================================================
-
-CREATE OR REPLACE FUNCTION fn_calculate_payment(
-    p_type_id INT,
-    p_checkin DATE,
-    p_checkout DATE
-)
-RETURNS NUMERIC AS $$
-DECLARE
-    v_days INT;
-    v_base_rate NUMERIC(12, 2);
-BEGIN
-    SELECT base_rate INTO v_base_rate
-    FROM room_type
-    WHERE id = p_type_id;
-
-    IF NOT FOUND THEN
-        RAISE EXCEPTION 'Room type % does not exist', p_type_id;
-    END IF;
-
-    -- for same-day checkin and checkout, the minimum would be 1 night
-    v_days := GREATEST(1, p_checkout - p_checkin);
-    RETURN v_base_rate * v_days;
-END;
-$$ LANGUAGE plpgsql;
-
--- ====================================================================================
--- FUNCTION fn_create_reservation_base creates the appropriate reservation row.
+-- FUNCTION fn_create_reservation_base creates a reservation entry.
 -- ====================================================================================
 
 CREATE OR REPLACE FUNCTION fn_create_reservation_base(
+    -- reservation attributes
     p_guest_id BIGINT,
     p_checkin DATE,
     p_checkout DATE,
-    p_type_id INT,
     p_payment_method payment_method,
-    p_source reservation_source
+    p_source reservation_source,
+    -- room attribute
+    p_room_type_id INT
 )
 RETURNS BIGINT
 AS $$
@@ -102,7 +170,7 @@ DECLARE
     v_payment_amount NUMERIC(12, 2);
 BEGIN
     -- calculate total payment
-    v_payment_amount := fn_calculate_payment(p_type_id, p_checkin, p_checkout);
+    v_payment_amount := fn_calculate_payment(p_room_type_id, p_checkin, p_checkout);
 
     INSERT INTO reservation (
         guest_id,
@@ -123,43 +191,6 @@ BEGIN
     RETURNING id INTO v_reservation_id;
 
     RETURN v_reservation_id;
-END;
-$$ LANGUAGE plpgsql;
-
--- ====================================================================================
--- FUNCTION fn_find_available_room returns the first row that satisfies availability
--- according to the date range. The lowest room number is selected first.
--- ====================================================================================
-
-CREATE OR REPLACE FUNCTION fn_find_available_room(
-    p_hotel_id INT,
-    p_type_id INT,
-    p_checkin DATE,
-    p_checkout DATE
-)
-RETURNS TABLE (hotel_id INT, room_number INT)
-AS $$
-BEGIN
-    RETURN QUERY
-    SELECT r.hotel_id, r.number
-    FROM room r
-    WHERE r.hotel_id = p_hotel_id
-        AND r.room_type_id = p_type_id
-        AND r.status_code = 'V/C' -- consider V/C as an available room
-        AND NOT EXISTS (
-            SELECT 1
-            FROM registration reg
-            JOIN reservation res ON res.id = reg.reservation_id
-            -- also check if a room number is already registered and if checkin/checkout
-            -- dates clash
-            WHERE reg.hotel_id = r.hotel_id
-                AND reg.room_number = r.number
-                AND res.checkin_date < p_checkout
-                AND res.checkout_date > p_checkin
-    )
-    ORDER BY r.number
-    FOR UPDATE SKIP LOCKED -- concurrency check
-    LIMIT 1;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -189,90 +220,72 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- ====================================================================================
--- FUNCTION create_reservation_workflow is the top-level workflow that creates 
--- reservations and other appropriate entries in the person, guest, and registration
--- tables. For the first room created for a reservation, pass in NULL for p_reservation_id.
--- Since it returns the reservation id, it can be reused in subsequent calls to
--- add to that same reservation.
+-- FUNCTION fn_reservation_workflow creates a reservation and its associated
+-- registration. It also adds a registration to an existing reservation if passed
+-- an existing reservation id.
 -- ====================================================================================
 
-CREATE OR REPLACE FUNCTION fn_create_reservation_workflow(
-    p_hotel_id INT,
-    p_passport TEXT,
-    p_contact_email TEXT,
-    p_contact_phone TEXT,
-    p_name TEXT,
-    p_gender TEXT,
-    p_street TEXT,
-    p_city TEXT,
-    p_country TEXT,
-    p_type_id INT,
+CREATE OR REPLACE FUNCTION fn_reservation_workflow(
+    -- reservation attributes
+    p_guest_id BIGINT,
     p_checkin DATE,
     p_checkout DATE,
     p_payment_method payment_method,
     p_source reservation_source,
+    -- registration attribute
+    p_hotel_id INT,
+    -- room attribute
+    p_room_type_id INT,
+    -- for multiple registrations to one reservation
     p_reservation_id BIGINT DEFAULT NULL
 )
 RETURNS BIGINT
 AS $$
 DECLARE
-    v_guest_id BIGINT;
     v_reservation_id BIGINT;
     v_room RECORD;
 BEGIN
-    -- retrieve the id of an existing guest or a newly created one
-    v_guest_id := fn_get_or_create_guest(
-        p_passport,
-        p_contact_email,
-        p_contact_phone,
-        p_name,
-        p_gender,
-        p_street,
-        p_city,
-        p_country
-    );
-
-    -- create a new reservation if not provided
+    -- create a new reservation if p_reservation_id was not provided
     IF p_reservation_id IS NULL THEN
         v_reservation_id := fn_create_reservation_base(
-            v_guest_id,
+            p_guest_id,
             p_checkin,
             p_checkout,
-            p_type_id,
             p_payment_method,
-            p_source
+            p_source,
+            p_room_type_id
         );
-    ELSE -- check if the reservation_id passed in belongs to the guest
+    ELSE -- if p_reservation_id was provided
+        -- check if the reservation id passed corresponds to the guest id passed
         SELECT id INTO v_reservation_id
         FROM reservation
         WHERE id = p_reservation_id
-            AND guest_id = v_guest_id;
+            AND guest_id = p_guest_id;
 
         IF NOT FOUND THEN
             RAISE EXCEPTION
-                'Reservation % does not belong to guest %',
-                p_reservation_id, v_guest_id;
+                '[reservation-guest-mismatch] Reservation % does not belong to guest %',
+                p_reservation_id, p_guest_id;
         END IF;
     END IF;
 
-    -- find available room
+    -- get available room of the specified room type that does not have conflicting dates
     SELECT * INTO v_room
     FROM fn_find_available_room(
         p_hotel_id,
-        p_type_id,
+        p_room_type_id,
         p_checkin,
         p_checkout
     );
 
-    -- return an error in case there are no more available rooms of the requested type
+    -- return an error if there are no available rooms
     IF NOT FOUND THEN
         RAISE EXCEPTION 
-            '[room-avail-err] No available room of type % for requested dates.',
-            p_type_id;
+            '[no-available-room] No available room of type % for requested dates',
+            p_room_type_id;
     END IF;
 
-    -- create the registration entry
-    -- PERFORM is used since the return value is not needed
+    -- create the registration entry (PERFORM is used since the return value is not needed)
     PERFORM fn_create_registration(
         v_reservation_id,
         v_room.hotel_id,
